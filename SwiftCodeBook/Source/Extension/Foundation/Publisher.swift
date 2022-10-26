db6b4c1213c06e2c21b0e7110c1f607244746d3e
yuman <yuman@microsoft.com>
2022-10-26 15:50:32 +0800

    update

---
+++ SwiftCodeBook/Source/Extension/Foundation/Publisher.swift
@@ -0,0 +1,22 @@
+//
+//  Publisher.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2022/10/26.
+//
+
+import Combine
+
+extension Publisher {
+    func sinkToResult(_ result: @escaping (Result<Output, Failure>) -> Void) -> AnyCancellable {
+        return sink(receiveCompletion: { completion in
+            switch completion {
+            case let .failure(error):
+                result(.failure(error))
+            default: break
+            }
+        }, receiveValue: { value in
+            result(.success(value))
+        })
+    }
+}
