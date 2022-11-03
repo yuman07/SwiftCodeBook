d2cff9eb347d7a4ed12985b4050eb3f576ee47c2
yuman <yuman@microsoft.com>
2022-11-03 15:29:21 +0800

    update code

---
+++ SwiftCodeBook/Source/Extension/Foundation/URL+Tools.swift
@@ -0,0 +1,16 @@
+//
+//  URL+Tools.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2022/11/3.
+//
+
+import Foundation
+
+extension URL {
+    var queryDictionary: [String: String] {
+        (URLComponents(string: absoluteString)?.queryItems ?? []).reduce(into: [:]) {
+            if let value = $1.value { $0[$1.name] = value }
+        }
+    }
+}
