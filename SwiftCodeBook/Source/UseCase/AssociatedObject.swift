6325747c97404be58e6419140e3a05620493146b
yuman <yuman@microsoft.com>
2022-11-04 14:59:42 +0800

    update code

---
+++ SwiftCodeBook/Source/UseCase/AssociatedObject.swift
@@ -0,0 +1,45 @@
+//
+//  AssociatedObject.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2022/11/4.
+//
+
+import Foundation
+
+class AssociatedObjectTest {}
+
+extension AssociatedObjectTest {
+    private struct AssociatedKeys {
+        static var age = "age"
+        static var name = "name"
+        static var block = "block"
+    }
+    
+    var age: Int {
+        get {
+            (objc_getAssociatedObject(self, &AssociatedKeys.age) as? Int) ?? 0
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.age, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    var name: String {
+        get {
+            (objc_getAssociatedObject(self, &AssociatedKeys.name) as? String) ?? ""
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.name, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    var block: (() -> Void)? {
+        get {
+            objc_getAssociatedObject(self, &AssociatedKeys.block) as? (() -> Void)
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.block, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+}
