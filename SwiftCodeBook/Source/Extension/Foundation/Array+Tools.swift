aa94f96cbe4d3710d3e0e236b616cb3101414bf6
yuman <yuman@microsoft.com>
2022-11-11 16:44:23 +0800

    update code

--- SwiftCodeBook/Source/Extension/Foundation/Array+Tools.swift
+++ SwiftCodeBook/Source/Extension/Foundation/Array+Tools.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
 extension Array where Element: Hashable {
     func unique() -> [Element] {
         guard count > 1 else { return self }
