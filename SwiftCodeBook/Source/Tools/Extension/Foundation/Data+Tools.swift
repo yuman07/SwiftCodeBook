5dcd16cccfc1482e012b948c1d05524f49edbbb1
yuman <yuman@microsoft.com>
2022-11-24 15:29:46 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/Data+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/Data+Tools.swift
@@ -8,11 +8,22 @@
 import Foundation
 
 extension Data {
-    func toZipped() -> Data? {
-        try? (self as NSData).compressed(using: .zlib) as Data
+    var isZipped: Bool {
+        guard count >= 2 else { return false }
+        return self[0] == 0x1f && self[1] == 0x8b
     }
     
-    func toUnZipped() -> Data? {
-        try? (self as NSData).decompressed(using: .zlib) as Data
+    func toZipped(zipAnyway: Bool = false) -> Data? {
+        if !zipAnyway && isZipped { return self }
+        return try? (self as NSData).compressed(using: .zlib) as Data
+    }
+    
+    func toUnZipped(allowRecursive: Bool = true) -> Data? {
+        var data: Data? = self
+        while let d = data, d.isZipped {
+            data = try? (self as NSData).decompressed(using: .zlib) as Data
+            guard allowRecursive else { break }
+        }
+        return data
     }
 }
