8bd6aa657e6541812e60ed00130549c11dccf92f
yuman <yuman@microsoft.com>
2022-11-24 12:12:21 +0800

    update code

---
+++ SwiftCodeBook/Source/Tools/Foundation/ThreadSafe/CancelBag.swift
@@ -0,0 +1,38 @@
+//
+//  CancelBag.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2022/11/24.
+//
+
+import Combine
+import Foundation
+
+public final class CancelBag {
+    private var tokens = Set<AnyCancellable>()
+    private let lock = NSRecursiveLock()
+
+    public init() {}
+
+    deinit {
+        cancel()
+    }
+
+    public func cancel() {
+        lock.lock(); defer { lock.unlock() }
+        for token in tokens { token.cancel() }
+        tokens.removeAll()
+    }
+
+    public func store(_ cancellable: AnyCancellable) {
+        lock.lock(); defer { lock.unlock() }
+        tokens.insert(cancellable)
+    }
+}
+
+public extension AnyCancellable {
+    func store(in cancelBag: CancelBag) {
+        cancelBag.store(self)
+    }
+}
+
