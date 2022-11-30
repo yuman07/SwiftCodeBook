623ae28df55ea483cd0dd509d83e6e9e50928817
yuman <yuman@microsoft.com>
2022-11-30 11:42:03 +0800

    update code

--- SwiftCodeBook/ViewController.swift
+++ SwiftCodeBook/ViewController.swift
@@ -12,6 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        print(UIDevice.current.applicationUsedMemory ?? 0)
     }
 }
