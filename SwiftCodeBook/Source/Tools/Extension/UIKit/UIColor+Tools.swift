49909fb72af551de2117584c8c42ba032a9a870e
yuman <yuman@microsoft.com>
2023-01-13 15:10:30 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/UIKit/UIColor+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/UIKit/UIColor+Tools.swift
@@ -8,20 +8,22 @@
 import UIKit
 
 public extension UIColor {
+    convenience init(hex: UInt64) {
+        self.init(red: CGFloat((hex & 0xff000000) >> 24) / 255,
+                  green: CGFloat((hex & 0x00ff0000) >> 16) / 255,
+                  blue: CGFloat((hex & 0x0000ff00) >> 8) / 255,
+                  alpha: CGFloat(hex & 0x000000ff) / 255)
+    }
+    
     convenience init?(RGBA: String) {
         guard RGBA[RGBA.startIndex] == "#" && (RGBA.count == 7 || RGBA.count == 9) else { return nil }
-        
         var hexNum = UInt64.zero
         let target = "\(RGBA.suffix(RGBA.count - 1))" + (RGBA.count == 7 ? "FF" : "" )
         guard case let s = Scanner(string: target), s.scanHexInt64(&hexNum) && s.isAtEnd else { return nil }
-        let r = CGFloat((hexNum & 0xff000000) >> 24) / 255
-        let g = CGFloat((hexNum & 0x00ff0000) >> 16) / 255
-        let b = CGFloat((hexNum & 0x0000ff00) >> 8) / 255
-        let a = CGFloat(hexNum & 0x000000ff) / 255
-        self.init(red: r, green: g, blue: b, alpha: a)
+        self.init(hex: hexNum)
     }
     
-    var rgbaTuple: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
         var r = CGFloat.zero
         var g = CGFloat.zero
         var b = CGFloat.zero
