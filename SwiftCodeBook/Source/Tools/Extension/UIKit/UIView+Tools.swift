de6e34e899903791dcd03f099071c9ab2169d572
yuman <yuman@microsoft.com>
2023-01-30 15:51:01 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/UIKit/UIView+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/UIKit/UIView+Tools.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 public extension UIView {
+    func addSubviews(_ views: [UIView]) {
+        views.forEach { self.addSubview($0) }
+    }
+    
     func removeAllSubviews() {
         while let last = subviews.last {
             last.removeFromSuperview()
+        }
+    }
+    
+    func removeAllGestureRecognizers() {
+        while let last = gestureRecognizers?.last {
+            removeGestureRecognizer(last)
         }
     }
     
@@ -23,5 +33,40 @@ public extension UIView {
             parentResponder = parentResponder?.next
         }
         return nil
+    }
+}
+
+private extension UIGestureRecognizer {
+    struct AssociatedKeys {
+        static var action = "action"
+    }
+    
+    var action: ((UIGestureRecognizer) -> Void)? {
+        get {
+            objc_getAssociatedObject(self, &AssociatedKeys.action) as? ((UIGestureRecognizer) -> Void)
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.action, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+}
+
+public extension UIView {
+    func addTapGesture(numberOfTapsRequired: Int = 1, action: @escaping (UIGestureRecognizer) -> Void) {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(gestureAction(gr:)))
+        tap.numberOfTapsRequired = numberOfTapsRequired
+        tap.action = action
+        addGestureRecognizer(tap)
+    }
+    
+    func addLongPressGesture(action: @escaping (UIGestureRecognizer) -> Void) {
+        let long = UILongPressGestureRecognizer(target: self, action: #selector(gestureAction(gr:)))
+        long.action = action
+        addGestureRecognizer(long)
+    }
+    
+    @objc
+    private func gestureAction(gr: UIGestureRecognizer) {
+        gr.action?(gr)
     }
 }
