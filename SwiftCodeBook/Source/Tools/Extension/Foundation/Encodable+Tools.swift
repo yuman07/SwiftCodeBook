0acdccf1e2318b8ff1f418c81f6cff414c0fadb7
yuman <yuman@microsoft.com>
2023-02-15 10:45:39 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/Encodable+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/Encodable+Tools.swift
@@ -8,24 +8,28 @@
 import Foundation
 
 public extension Encodable {
-    func toJSONData() -> Data? {
+    private func toJSONDataAndObj() -> (data: Data, obj: Any)? {
         if let data = self as? Data {
-            return (try? JSONSerialization.jsonObject(with: data)).flatMap { _ in data }
+            return (try? JSONSerialization.jsonObject(with: data)).flatMap { (data, $0) }
         } else if let string = self as? String  {
-            return string.data(using: .utf8)?.toJSONData()
+            return string.data(using: .utf8)?.toJSONDataAndObj()
         } else {
-            return try? JSONEncoder().encode(self)
+            return (try? JSONEncoder().encode(self))?.toJSONDataAndObj()
         }
     }
     
+    func toJSONData() -> Data? {
+        toJSONDataAndObj()?.data
+    }
+    
     func toJSONArray() -> [Any]? {
-        guard let data = toJSONData() else { return nil }
-        return try? JSONSerialization.jsonObject(with: data) as? [Any]
+        guard let array = toJSONDataAndObj()?.obj as? [Any] else { return nil }
+        return array
     }
     
     func toJSONDictionary() -> [String: Any]? {
-        guard let data = toJSONData() else { return nil }
-        return try? JSONSerialization.jsonObject(with: data) as? [String: Any]
+        guard let dict = toJSONDataAndObj()?.obj as? [String: Any] else { return nil }
+        return dict
     }
     
     func toJSONString() -> String? {
