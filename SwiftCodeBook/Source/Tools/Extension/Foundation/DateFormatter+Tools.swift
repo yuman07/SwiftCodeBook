53e6be43cbbebf6a0e8293aae0f73dd5afe629c8
yuman <yuman@microsoft.com>
2023-02-16 11:46:45 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/DateFormatter+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/DateFormatter+Tools.swift
@@ -29,6 +29,22 @@ public extension DateFormatter {
         }(),
         {
             let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+            formatter.calendar = Calendar(identifier: .iso8601)
+            formatter.timeZone = TimeZone(secondsFromGMT: 0)
+            formatter.locale = Locale(identifier: kUSPosixLocaleID)
+            return formatter
+        }(),
+        {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+            formatter.calendar = Calendar(identifier: .iso8601)
+            formatter.timeZone = TimeZone(secondsFromGMT: 0)
+            formatter.locale = Locale(identifier: kUSPosixLocaleID)
+            return formatter
+        }(),
+        {
+            let formatter = DateFormatter()
             formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
             formatter.calendar = Calendar(identifier: .iso8601)
             formatter.timeZone = TimeZone(secondsFromGMT: 0)
