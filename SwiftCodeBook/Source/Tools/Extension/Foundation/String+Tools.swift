94038e74e9445384bee2dc99139895cd4a5282be
yuman <yuman@microsoft.com>
2023-02-16 15:51:46 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/String+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/String+Tools.swift
@@ -8,52 +8,38 @@
 import Foundation
 
 public extension String {
-    /// Safely subscript Character with index.
-    ///
-    ///     let value = "Abcdef123456"
-    ///     value[3] // "d"
-    ///     value[99] // nil
-    ///
-    /// - Parameter i: index.
     subscript(_ position: Int) -> Character? {
-        guard 0..<count ~= position else { return nil }
+        guard 0 ..< count ~= position else { return nil }
         return self[index(startIndex, offsetBy: position)]
     }
-
-    /// Safely subscript string within a closed range.
-    ///
-    ///     let value = "Abcdef123456"
-    ///     value[3...6] // "def1"
-    ///     value[3...99] // "def123456"
-    ///     value[99...199] // nil
-    ///
-    /// - Parameter range: Closed range.
+    
     subscript(_ range: ClosedRange<Int>) -> Substring? {
-        guard let lowerIndex = index(startIndex, offsetBy: max(0, range.lowerBound), limitedBy: endIndex),
-              let upperIndex = index(lowerIndex, offsetBy: range.upperBound - range.lowerBound + 1, limitedBy: endIndex)
-        else {
-            guard range.lowerBound < count else { return nil }
-            return dropFirst(range.lowerBound)
-        }
+        guard !isEmpty && range.lowerBound < count && range.upperBound >= 0 else { return nil }
+        let lowerIndex = index(startIndex, offsetBy: max(0, range.lowerBound))
+        let upperIndex = index(startIndex, offsetBy: min(count - 1, range.upperBound))
+        return self[lowerIndex...upperIndex]
+    }
+    
+    subscript(_ range: CountableRange<Int>) -> Substring? {
+        guard !isEmpty && range.lowerBound < count && range.upperBound > 0 else { return nil }
+        let lowerIndex = index(startIndex, offsetBy: max(0, range.lowerBound))
+        let upperIndex = index(startIndex, offsetBy: min(count, range.upperBound))
         return self[lowerIndex..<upperIndex]
     }
-
-    /// Safely subscript string within a half-open range.
-    ///
-    ///     let value = "Abcdef123456"
-    ///     value[3..<6] // "def"
-    ///     value[3..<99] // "def123456"
-    ///     value[99..<199] // nil
-    ///
-    /// - Parameter range: Half-open range.
-    subscript(_ range: CountableRange<Int>) -> Substring? {
-        guard let lowerIndex = index(startIndex, offsetBy: max(0, range.lowerBound), limitedBy: endIndex),
-              let upperIndex = index(lowerIndex, offsetBy: range.upperBound - range.lowerBound, limitedBy: endIndex)
-        else {
-            guard range.lowerBound < count else { return nil }
-            return dropFirst(range.lowerBound)
-        }
-        return self[lowerIndex..<upperIndex]
+    
+    subscript(_ range: PartialRangeFrom<Int>) -> Substring? {
+        guard case let len = count - range.lowerBound, len > 0 else { return nil }
+        return suffix(len)
+    }
+    
+    subscript(_ range: PartialRangeUpTo<Int>) -> Substring? {
+        guard case let len = range.upperBound - 1, len > 0 else { return nil }
+        return prefix(len)
+    }
+    
+    subscript(_ range: PartialRangeThrough<Int>) -> Substring? {
+        guard case let len = range.upperBound, len >= 0 else { return nil }
+        return prefix(len)
     }
 }
 
