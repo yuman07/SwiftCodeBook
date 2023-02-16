a38e9aacd6b1d5f6733fb6541e9674324c125848
yuman <yuman@microsoft.com>
2023-02-16 15:52:15 +0800

    update code

--- SwiftCodeBook/ViewController.swift
+++ SwiftCodeBook/ViewController.swift
@@ -12,8 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let s = "123"
-        let sss = [11, 22, 33]
-        print(s[...0])
+        
     }
 }
