4bae8cfbb9d8f387ce66a6a257d6c962e66832fc
yuman <yuman@microsoft.com>
2023-02-28 15:14:34 +0800

    update code

---
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/NSRange+Tools.swift
@@ -0,0 +1,14 @@
+//
+//  NSRange+Tools.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2023/2/28.
+//
+
+import Foundation
+
+public extension NSRange {
+    var isValid: Bool {
+        location >= 0 && location != NSNotFound && length > 0
+    }
+}
--- SwiftCodeBook/Source/UseCase/UIKit/UITextView.clickSomeText.swift
+++ SwiftCodeBook/Source/UseCase/UIKit/UITextView.clickSomeText.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 private final class ClickTextVC: UIViewController {
-    private var rects = [CGRect]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let textView = UITextView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         let attString = NSMutableAttributedString(string: "你好啊你好啊你好啊你好啊你好啊你好啊，详情请参阅《个人信息保护法》的相关规定")
-        let range = NSRange(location: 24, length: 9)
+        guard case let range = (attString.string as NSString).range(of: "《个人信息保护法》"), range.isValid else { return }
         attString.addAttributes([.foregroundColor: UIColor.blue], range: range)
         textView.attributedText = attString
         view.addSubview(textView)
         
-        textView.addTapGesture { [weak self] gr in
-            guard let self, self.rects.contains(where: { $0.contains(gr.location(in: gr.view)) }) else { return }
-            print("clicked!!!")
-        }
+        var rects = [CGRect]()
         
         guard let start = textView.position(from: textView.beginningOfDocument, offset: range.location),
               let end = textView.position(from: start, offset: range.length),
@@ -31,5 +26,10 @@ private final class ClickTextVC: UIViewController {
         else { return }
         
         rects = textView.selectionRects(for: textRange).map { $0.rect }
+        
+        textView.addTapGesture { gr in
+            guard rects.contains(where: { $0.contains(gr.location(in: gr.view)) }) else { return }
+            print("clicked!!!")
+        }
     }
 }
