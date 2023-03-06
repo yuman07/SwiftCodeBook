2ec278d3b6955883834cb71c4197ab0f08e4a415
yuman <yuman@microsoft.com>
2023-03-06 14:50:46 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/String+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/String+Tools.swift
@@ -50,18 +50,6 @@ public extension String {
 }
 
 public extension String {
-    func allIndicesOf(string: String) -> [Int] {
-        var indices = [Int]()
-        var start = startIndex
-        while start < endIndex, let range = range(of: string, range: start..<endIndex), !range.isEmpty {
-            indices.append(distance(from: startIndex, to: range.lowerBound))
-            start = range.upperBound
-        }
-        return indices
-    }
-}
-
-public extension String {
     var containsChinese: Bool {
         contains { $0.isChinese }
     }
