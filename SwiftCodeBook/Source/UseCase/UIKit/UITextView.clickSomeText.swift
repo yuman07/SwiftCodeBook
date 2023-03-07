dfcc31eb29ac5bd5effc9da9b9bca570b3c12ab1
yuman <yuman@microsoft.com>
2023-03-07 15:57:48 +0800

    update code

--- SwiftCodeBook/Source/UseCase/UIKit/UITextView.clickSomeText.swift
+++ SwiftCodeBook/Source/UseCase/UIKit/UITextView.clickSomeText.swift
@@ -12,27 +12,24 @@ private final class ClickTextVC: UIViewController {
         super.viewDidLoad()
         
         let textView = UITextView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        textView.isSelectable = false
         let attString = NSMutableAttributedString(string: "你好啊你好啊你好啊你好啊你好啊你好啊，详情请参阅《个人信息保护法》的相关规定")
         guard case let range = (attString.string as NSString).range(of: "《个人信息保护法》"), range.isValid else { return }
         attString.addAttributes([.foregroundColor: UIColor.blue], range: range)
         textView.attributedText = attString
         view.addSubview(textView)
         
-        var rects = [CGRect]()
-        
-        guard let start = textView.position(from: textView.beginningOfDocument, offset: range.location),
-              let end = textView.position(from: start, offset: range.length),
-              let textRange = textView.textRange(from: start, to: end)
-        else { return }
-        
-        // 注意：这个计算必须满足此时textView已经layout finished(显然rects和textView的size等有关)
-        // 这里为了方便，就使用了手动布局，因此可以直接计算
-        // 如果是自动布局，则可以把该方法放到比如viewDidLayoutSubviews()中
-        rects = textView.selectionRects(for: textRange).map { $0.rect }
-        
         textView.addTapGesture { gr in
-            guard rects.contains(where: { $0.contains(gr.location(in: gr.view)) }) else { return }
-            print("clicked!!!")
+            guard let textView = gr.view as? UITextView,
+                  case let location = gr.location(in: gr.view),
+                  let start = textView.position(from: textView.beginningOfDocument, offset: range.location),
+                  let end = textView.position(from: start, offset: range.length),
+                  let textRange = textView.textRange(from: start, to: end),
+                  case let rects = textView.selectionRects(for: textRange).map({ $0.rect }),
+                  rects.contains(where: { $0.contains(location) })
+            else { return }
+            
+            print("clicked")
         }
     }
 }
