67cef6efe4f96916c5ec9eb1660049b0663ffaf8
yuman <yuman@microsoft.com>
2023-03-07 16:20:24 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/UIKit/UIView+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/UIKit/UIView+Tools.swift
@@ -50,21 +50,21 @@ private extension UIGestureRecognizer {
 
 public extension UIView {
     func addTapGesture(numberOfTapsRequired: Int = 1, action: @escaping (UIGestureRecognizer) -> Void) {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(gestureAction(gr:)))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(gestureAction(gesture:)))
         tap.numberOfTapsRequired = numberOfTapsRequired
         tap.action = action
         addGestureRecognizer(tap)
     }
     
     func addLongPressGesture(action: @escaping (UIGestureRecognizer) -> Void) {
-        let long = UILongPressGestureRecognizer(target: self, action: #selector(gestureAction(gr:)))
+        let long = UILongPressGestureRecognizer(target: self, action: #selector(gestureAction(gesture:)))
         long.action = action
         addGestureRecognizer(long)
     }
     
     @objc
-    private func gestureAction(gr: UIGestureRecognizer) {
-        gr.action?(gr)
+    private func gestureAction(gesture: UIGestureRecognizer) {
+        gesture.action?(gesture)
     }
 }
 
