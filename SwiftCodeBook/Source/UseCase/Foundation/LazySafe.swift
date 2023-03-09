b8f08f1815c042c3786b036ced8046353aa23886
yuman <yuman@microsoft.com>
2023-03-09 14:28:45 +0800

    update code

--- SwiftCodeBook/Source/UseCase/Foundation/LazySafe.swift
+++ SwiftCodeBook/Source/UseCase/Foundation/LazySafe.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
+// 这里仅保证view的懒加载过程是线程安全的
+// 注意set/get仍不是线程安全的
 class LazySafe {
-    var view: UIView {
+    lazy var view: UIView = {
         enum Once {
             static var view = {
                 UIView()
             }()
         }
         return Once.view
-    }
+    }()
 }
