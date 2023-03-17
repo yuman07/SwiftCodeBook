b5ace170e9255c4f9d93421866206c4f2be304c6
yuman <yuman@microsoft.com>
2023-03-17 12:57:18 +0800

    update code

--- SwiftCodeBook/Source/Tools/UIKit/View/GradientView.swift
+++ SwiftCodeBook/Source/Tools/UIKit/View/GradientView.swift
@@ -12,6 +12,8 @@ public final class GradientView: UIView {
         CAGradientLayer.self
     }
     
+    // point的坐标系和UIKit相同，且取值范围是[0, 1]
+    // 即(0, 0)代表左上角，(1, 1)代表右下角
     public func updateGradient(startPoint: CGPoint, endPoint: CGPoint, startColor: UIColor, endColor: UIColor) {
         guard let layer = self.layer as? CAGradientLayer else { return }
         layer.startPoint = startPoint
