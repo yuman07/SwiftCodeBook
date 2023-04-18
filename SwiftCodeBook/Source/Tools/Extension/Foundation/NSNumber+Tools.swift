192e6f31736859ebc636506462bc8a9f70a8ecab
yuman <yuman@microsoft.com>
2023-04-18 14:36:37 +0800

    update code

---
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/NSNumber+Tools.swift
@@ -0,0 +1,14 @@
+//
+//  NSNumber+Tools.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2023/4/18.
+//
+
+import UIKit
+
+extension NSNumber {
+    public var CGFloatValue: CGFloat {
+        CGFloat(doubleValue)
+    }
+}
