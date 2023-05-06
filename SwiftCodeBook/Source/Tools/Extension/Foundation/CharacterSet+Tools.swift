8e7c3040805d5830bfe57ba07aa5212eb4d04bb2
yuman <yuman@microsoft.com>
2023-05-06 14:04:38 +0800

    update code

---
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/CharacterSet+Tools.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterSet+Tools.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2023/5/6.
+//
+
+import Foundation
+
+public extension CharacterSet {
+    // https://en.wikipedia.org/wiki/Zero-width_space
+    // https://unicode-explorer.com/search/
+    static let zeroWidthSpace = CharacterSet(charactersIn: "\u{200B}\u{2060}\u{200C}\u{FEFF}\u{200D}")
+    
+    static let whitespacesNewlineAndZeroWidthSpace = {
+        var set = CharacterSet.whitespacesAndNewlines
+        set.formUnion(.zeroWidthSpace)
+        return set
+    }()
+}
