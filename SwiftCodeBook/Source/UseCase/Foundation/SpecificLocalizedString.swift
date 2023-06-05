5d0d1dae003003bdf1b0038a1114456f448f7272
yuman <yuman@microsoft.com>
2023-06-05 14:31:17 +0800

    update code

--- SwiftCodeBook/Source/UseCase/Foundation/SpecificLocalizedString.swift
+++ SwiftCodeBook/Source/UseCase/Foundation/SpecificLocalizedString.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 // 有时我们需要在多语言的情况下，获取某种特定语言下的字符串(这里以要获取简体中文为例)
-private func simplifiedChineseLocalizedString(with key: String) -> String? {
+private func simplifiedChineseLocalizedString(with key: String) -> String {
     guard let path = Bundle.main.path(forResource: "zh-Hans", ofType: "lproj"),
-          let bundle = Bundle(path: path) else { return nil }
+          let bundle = Bundle(path: path) else { return key }
     return bundle.localizedString(forKey: key, value: nil, table: nil)
 }
