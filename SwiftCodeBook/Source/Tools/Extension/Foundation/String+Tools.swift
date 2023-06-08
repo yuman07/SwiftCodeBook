977f593a0f6173115bc233acc18ed124fa00766a
yuman <yuman@microsoft.com>
2023-06-08 16:16:38 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/String+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/String+Tools.swift
@@ -14,32 +14,57 @@ public extension String {
     }
     
     subscript(_ range: ClosedRange<Int>) -> Substring? {
-        guard !isEmpty && range.lowerBound < count && range.upperBound >= 0 else { return nil }
-        let lowerIndex = index(startIndex, offsetBy: max(0, range.lowerBound))
-        let upperIndex = index(startIndex, offsetBy: min(count - 1, range.upperBound))
-        return self[lowerIndex...upperIndex]
+        guard !isEmpty && range.lowerBound >= 0 && range.upperBound < count,
+              let lowerIndex = index(startIndex, offsetBy: range.lowerBound, limitedBy: endIndex),
+              let upperIndex = index(startIndex, offsetBy: range.upperBound, limitedBy: endIndex)
+        else { return nil }
+        return self[lowerIndex ... upperIndex]
     }
     
     subscript(_ range: CountableRange<Int>) -> Substring? {
-        guard !isEmpty && range.lowerBound < count && range.upperBound > 0 else { return nil }
-        let lowerIndex = index(startIndex, offsetBy: max(0, range.lowerBound))
-        let upperIndex = index(startIndex, offsetBy: min(count, range.upperBound))
-        return self[lowerIndex..<upperIndex]
+        guard !isEmpty && range.lowerBound >= 0 && range.upperBound <= count,
+              let lowerIndex = index(startIndex, offsetBy: range.lowerBound, limitedBy: endIndex),
+              let upperIndex = index(startIndex, offsetBy: range.upperBound, limitedBy: endIndex)
+        else { return nil }
+        return self[lowerIndex ..< upperIndex]
     }
     
+    // [0...]
     subscript(_ range: PartialRangeFrom<Int>) -> Substring? {
-        guard case let len = count - range.lowerBound, len > 0 else { return nil }
+        guard case let len = count - range.lowerBound, len > 0, range.lowerBound >= 0 else { return nil }
         return suffix(len)
     }
     
+    // [..<2]
     subscript(_ range: PartialRangeUpTo<Int>) -> Substring? {
         guard case let len = range.upperBound - 1, len > 0 else { return nil }
         return prefix(len)
     }
     
+    // [...2]
     subscript(_ range: PartialRangeThrough<Int>) -> Substring? {
         guard case let len = range.upperBound, len >= 0 else { return nil }
         return prefix(len)
+    }
+}
+
+public extension String {
+    func indexSafely(after idx: Index) -> Index? {
+        guard idx < endIndex else { return nil }
+        return index(after: idx)
+    }
+    
+    func indexSafely(before idx: Index) -> Index? {
+        guard idx > startIndex else { return nil }
+        return self.index(before: idx)
+    }
+    
+    func firstIndex(of char: Character, after idx: Index) -> Index? {
+        indexSafely(after: idx).flatMap { self[$0 ..< endIndex].firstIndex(of: char) }
+    }
+    
+    func lastIndex(of char: Character, before idx: Index) -> Index? {
+        indexSafely(before: idx).flatMap { self[startIndex ... $0].lastIndex(of: char) }
     }
 }
 
