ef0fa85eb56c8fe3ca7c145db15431f6264f9091
yuman <yuman@microsoft.com>
2024-04-26 15:45:33 +0800

    update code

---
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/Locale+Tools.swift
@@ -0,0 +1,17 @@
+//
+//  Locale+Tools.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2024/4/26.
+//
+
+import Foundation
+
+public extension Locale {
+    // "en-US"
+    var isoLanguageCode: String {
+        let localeIdentifier = identifier.replacingOccurrences(of: "_", with: "-")
+        /// Some locale identifiers contain special fields like calendar or numbers, eg: "ar-SA@calendar=gregorian;numbers=latn"
+        return localeIdentifier.components(separatedBy: "@").first ?? localeIdentifier
+    }
+}
