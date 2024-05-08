7fed7e9c87703cbf9f5872b61fbbd5a2e5f6a257
yuman <yuman@microsoft.com>
2024-05-08 09:32:42 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/ISO8601DateFormatter+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/ISO8601DateFormatter+Tools.swift
@@ -19,23 +19,21 @@ public extension ISO8601DateFormatter {
     
     static func string(from date: Date, options: ISO8601DateFormatter.Options = [.withTimeZone, .withFractionalSeconds]) -> String {
         var index = 0
-        optionals.enumerated().forEach { idx, value in
-            if options.contains(value) { index += (1 << idx) }
+        for (idx, value) in optionals.enumerated() where options.contains(value) {
+            index += (1 << idx)
         }
         return formatters[index].string(from: date)
     }
     
-    private static let basic: ISO8601DateFormatter.Options =
-    [.withYear, .withMonth, .withDay, .withTime, .withDashSeparatorInDate, .withColonSeparatorInTime, .withColonSeparatorInTimeZone]
+    private static let basic: ISO8601DateFormatter.Options = [.withYear, .withMonth, .withDay, .withTime, .withDashSeparatorInDate, .withColonSeparatorInTime, .withColonSeparatorInTimeZone]
     
-    private static let optionals: [ISO8601DateFormatter.Options] =
-    [.withTimeZone, .withFractionalSeconds, .withSpaceBetweenDateAndTime]
+    private static let optionals: [ISO8601DateFormatter.Options] = [.withTimeZone, .withFractionalSeconds, .withSpaceBetweenDateAndTime]
     
     private static let formatters = {
         (0 ..< 1 << optionals.count).map { num -> ISO8601DateFormatter in
             var formatOptions = basic
-            optionals.enumerated().forEach { index, value in
-                if (num >> index) & 1 == 1 { formatOptions.insert(value) }
+            for (index, value) in optionals.enumerated() where (num >> index) & 1 == 1 {
+                formatOptions.insert(value)
             }
             let formatter = ISO8601DateFormatter()
             formatter.formatOptions = formatOptions
