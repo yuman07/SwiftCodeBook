6c1895c84bc280384572c8c943443dcd100b2be7
yuman <yuman@microsoft.com>
2024-10-31 23:43:03 +0800

    update code

--- SwiftCodeBook/Source/UseCase/Foundation/SafeLazy.swift
+++ SwiftCodeBook/Source/UseCase/Foundation/SafeLazy.swift
@@ -12,7 +12,7 @@ import os
 // 1) 不是线程安全的，即如果多个线程同时访问会有问题
 // 2) 必须以var修饰，即lazy var这种。造成了是该value是可读写的，但有时我们仅想让该值只读
 // 其实可以不使用lazy，而是计算属性来解决以上需求且也是延迟初始化的
-final class LazyValue {
+final class SafeLazy {
     // 线程安全 + 只读
     private let readOnlyObjLock = OSAllocatedUnfairLock<NSObject?>(initialState: nil)
     var readOnlyObj: NSObject {
