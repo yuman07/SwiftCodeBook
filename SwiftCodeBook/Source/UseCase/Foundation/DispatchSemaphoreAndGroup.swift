a3728491081ab9ff48de17a7f96fe35d07a52809
yuman <yuman@microsoft.com>
2024-11-01 13:29:07 +0800

    update code

--- SwiftCodeBook/Source/UseCase/Foundation/DispatchSemaphoreAndGroup.swift
+++ SwiftCodeBook/Source/UseCase/Foundation/DispatchSemaphoreAndGroup.swift
@@ -13,14 +13,23 @@ final class DispatchSemaphoreAndGroup {
     // 有一个初始值(Int)，调用signal会将该值+1，调用wait会先将该值-1然后判断：如果小于0则同步等待直到值>=0或者超时
     // 注意点：
     // 1) 初始值必须非负
-    // 2) signal/wait尽量保持平衡，必须保证该DispatchSemaphore最终释放时的值不小于创建时的初始值
-    func ss() {
-        let a = DispatchSemaphore(value: 0)
-//        a.signal()
-//        a.wait()
-        a.signal()
-        print(a.wait(timeout: .now() + 2.0))
-        print(a.wait(timeout: .now() + 2.0))
-        print("123")
+    // 2) 当初始值大于0时，必须保证该DispatchSemaphore最终释放时的值不小于创建时的初始值
+    // 注意由于1/2的限制，强烈建议仅用0作为DispatchSemaphore的初始值
+    func testSemaphore() {
+        // 可以使用DispatchSemaphore来做线程同步
+        // 比如你需要阻塞以等待另一个异步操作
+        let semaphore = DispatchSemaphore(value: 0)
+        print("begin async")
+        DispatchQueue.global().async {
+            print("end async")
+            semaphore.signal()
+        }
+        let result = semaphore.wait(timeout: .now() + 2)
+        switch result {
+        case .success:
+            print("success")
+        case .timedOut:
+            print("timedOut")
+        }
     }
 }
