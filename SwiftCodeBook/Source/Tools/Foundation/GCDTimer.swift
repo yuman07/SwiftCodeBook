7d44deb3b7c22b39e2d18348f396b6312ff49af5
yuman <yuman@microsoft.com>
2024-11-10 19:15:41 +0800

    update code

--- SwiftCodeBook/Source/Tools/Foundation/GCDTimer.swift
+++ SwiftCodeBook/Source/Tools/Foundation/GCDTimer.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import os
 
 public final class GCDTimer: @unchecked Sendable {
     private enum State {
@@ -16,23 +15,20 @@ public final class GCDTimer: @unchecked Sendable {
         case stoped
     }
     
-    private let state = OSAllocatedUnfairLock(initialState: State.inited)
-    private let count = OSAllocatedUnfairLock(initialState: 0)
+    private var state = State.inited
+    private var count = 0
     private let timer: DispatchSourceTimer
     private let timeInterval: TimeInterval
+    private let queue = DispatchQueue(label: "SwiftCodeBook.GCDTimerQueue")
     
     public init(timeInterval: TimeInterval, repeats: Bool, queue: DispatchQueue = .main, block: @escaping (_ count: Int) -> Void) {
         self.timeInterval = timeInterval
         self.timer = DispatchSource.makeTimerSource(flags: .strict, queue: queue)
         self.timer.setEventHandler { [weak self] in
             guard let self else { return }
-            let curCount = count.withLock { count in
-                let curCount = count
-                count += 1
-                return curCount
-            }
-            block(curCount)
+            block(count)
             if !repeats { stop() }
+            count += 1
         }
     }
     
@@ -41,16 +37,22 @@ public final class GCDTimer: @unchecked Sendable {
     }
     
     public func start() {
-        state.withLock { state in
-            guard state == .inited || state == .paused else { return }
-            if state == .inited { timer.schedule(deadline: .now() + timeInterval, repeating: .milliseconds(Int(timeInterval * 1000))) }
+        queue.sync {
+            switch state {
+            case .running, .stoped:
+                return
+            case .paused:
+                break
+            case .inited:
+                timer.schedule(deadline: .now() + timeInterval, repeating: .milliseconds(Int(timeInterval * 1000)))
+            }
             timer.resume()
             state = .running
         }
     }
     
     public func pause() {
-        state.withLock { state in
+        queue.sync {
             guard state == .running else { return }
             timer.suspend()
             state = .paused
@@ -58,8 +60,9 @@ public final class GCDTimer: @unchecked Sendable {
     }
     
     public func stop() {
-        state.withLock { state in
+        queue.sync {
             guard state != .stoped else { return }
+            timer.setEventHandler(handler: nil)
             if state == .inited { timer.resume() }
             timer.cancel()
             state = .stoped
