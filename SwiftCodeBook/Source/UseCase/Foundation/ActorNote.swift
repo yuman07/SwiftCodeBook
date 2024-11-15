63588b90d6f4c9549d9ae0117c25349ee34c6419
yuman <yuman@microsoft.com>
2024-11-15 22:35:31 +0800

    update code

--- SwiftCodeBook/Source/UseCase/Foundation/ActorNote.swift
+++ SwiftCodeBook/Source/UseCase/Foundation/ActorNote.swift
@@ -9,11 +9,9 @@ import Foundation
 
 // Actor是可重入的，即调用Actor上的方法时，虽然是顺序执行
 // 但是如果某个方法中有await，执行该方法到await时，该方法会放弃当前线程，先hold，让后面的方法先执行
-// 截止目前Actor还不支持关闭该特性，虽然大部分情况可重入也没有问题，但也有一些场景我们需要它不可重入
-// 以下进行讨论并给出可能的解决方法
-
-// 需要不可重入的一个例子是某个Actor的初始化还需要调用一个async setup
-// 该Actor的所有方法都必须等这个setup执行完毕后才能执行
+// 在开发中我们有时会遇到一个需求：在actor初始化完毕后，还需要执行一个async的setup方法
+// 该actor的所有方法都必须等该setup结束后才能执行
+// 可以通过持有该setup的task来实现该需求：
 final class TestActorNote {
     func testSetupActor() {
         let test = TestSetupActor()
@@ -25,35 +23,24 @@ final class TestActorNote {
 
 private actor TestSetupActor {
     var file: Int?
-    
-    init() {
-        // 使用最高优先级确保setup()是该actor执行的第一个方法
-        Task(priority: .high) {
-            await setup()
+    lazy var setupTask: Task<Void, Never> = {
+        Task {
+            file = await setup()
         }
-    }
-    
-    // 利用semaphore和Task.detached，将异步转同步
-    // 注意这样做会有优先级翻转问题，仅作为万不得已的手段
-    func setup() {
-        let semaphore = DispatchSemaphore(value: 0)
-        var result: Int?
-        Task.detached(priority: Task.currentPriority) {
-            print("setup begin")
-            result = await read()
-            print("setup end")
-            semaphore.signal()
-        }
-        semaphore.wait()
-        file = result
-    }
-    
-    func play() {
+    }()
+
+    func play() async {
+        await setupTask.value
         print("play: \(file ?? 0)")
+    }
+    
+    func pause() async {
+        await setupTask.value
+        print("pause: \(file ?? 0)")
     }
 }
 
-private func read() async -> Int {
+private func setup() async -> Int {
     print("read begin")
     try? await Task.sleep(for: .seconds(2))
     print("read end")
