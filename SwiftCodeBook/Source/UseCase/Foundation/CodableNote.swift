261bd29786eb47679f4a334078e9888a41003845
yuman <yuman@microsoft.com>
2024-12-26 11:05:37 +0800

    update code

--- SwiftCodeBook/Source/UseCase/Foundation/CodableNote.swift
+++ SwiftCodeBook/Source/UseCase/Foundation/CodableNote.swift
@@ -8,14 +8,23 @@
 import Foundation
 
 func testOptionalCodable() {
-    struct YMModel: Codable {
+    struct YMModel1: Codable {
+        let values: [Int]
+    }
+    struct YMModel2: Codable {
         let values: [OptionalCodable<Int>]
     }
     
     let json = "{\"values\":[1,2,\"3\"]}"
-    let model = YMModel(JSONString: json)
-    if let model {
-        print(model)
-        print(model.toJSONString() ?? "fail")
+    let model1 = YMModel1(JSONString: json)
+    let model2 = YMModel2(JSONString: json)
+    if let model1 {
+        print(model1)
+    } else {
+        print("model1 fail")
+    }
+    if let model2 {
+        print(model2)
+        print(model2.toJSONString() ?? "fail")
     }
 }
