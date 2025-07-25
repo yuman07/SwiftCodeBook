800c2481d3ad9412afec60011217f84e35b0b4c6
yuman <yuman@microsoft.com>
2025-07-25 16:33:37 +0800

    update code

--- SwiftCodeBook/Source/Tools/Foundation/XMLNodeParser.swift
+++ SwiftCodeBook/Source/Tools/Foundation/XMLNodeParser.swift
@@ -60,14 +60,17 @@ private final class XMLNodeParserImp: NSObject, @unchecked Sendable {
 
     func parse() {
         guard let parser else {
-            stopWithError(NSError(reason: "DataSource error: Unable to initialize parser"))
+            stop(.failure(NSError(reason: "DataSource error: Unable to initialize parser")))
             return
         }
         parser.parse()
     }
     
-    func stopWithError(_ error: Error) {
-        continuation?.resume(throwing: error)
+    func stop(_ result: Result<XMLNode, Error>) {
+        switch result {
+        case let .success(node): continuation?.resume(with: .success(node))
+        case let .failure(error): continuation?.resume(throwing: error)
+        }
         continuation = nil
         parser?.abortParsing()
         parser = nil
@@ -78,11 +81,10 @@ private final class XMLNodeParserImp: NSObject, @unchecked Sendable {
 extension XMLNodeParserImp: XMLParserDelegate {
     func parserDidEndDocument(_ parser: XMLParser) {
         guard let root = stack.popLast(), stack.isEmpty else {
-            stopWithError(NSError(reason: "Parsing error: There should be only one root node."))
+            stop(.failure(NSError(reason: "Parsing error: There should be only one root node.")))
             return
         }
-        continuation?.resume(with: .success(root.toXMLNode()))
-        continuation = nil
+        stop(.success(root.toXMLNode()))
     }
     
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String] = [:]) {
@@ -94,7 +96,7 @@ extension XMLNodeParserImp: XMLParserDelegate {
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         guard var lastNode = stack.popLast() else {
-            stopWithError(NSError(reason: "Parsing error: No corresponding node was found when running foundCharacters"))
+            stop(.failure(NSError(reason: "Parsing error: No corresponding node was found when running foundCharacters")))
             return
         }
         if case let string = string.trimmingCharacters(in: .whitespacesAndNewlines), !string.isEmpty {
@@ -105,7 +107,7 @@ extension XMLNodeParserImp: XMLParserDelegate {
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         guard let lastNode = stack.popLast() else {
-            stopWithError(NSError(reason: "Parsing error: No corresponding node was found when running didEndElement"))
+            stop(.failure(NSError(reason: "Parsing error: No corresponding node was found when running didEndElement")))
             return
         }
         if var parentNode = stack.popLast() {
@@ -117,11 +119,11 @@ extension XMLNodeParserImp: XMLParserDelegate {
     }
     
     func parser(_ parser: XMLParser, parseErrorOccurred parseError: any Error) {
-        stopWithError(parseError)
+        stop(.failure(parseError))
     }
     
     func parser(_ parser: XMLParser, validationErrorOccurred validationError: any Error) {
-        stopWithError(validationError)
+        stop(.failure(validationError))
     }
 }
 
