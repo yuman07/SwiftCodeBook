95f674c36de35383db6db79562c64cae22b8e782
yuman <yuman@microsoft.com>
2025-09-18 11:22:09 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/Locale+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/Locale+Tools.swift
@@ -10,8 +10,7 @@ import Foundation
 public extension Locale {
     // "en-US"
     var bcp47Identifier: String {
-        let localeIdentifier = identifier.replacingOccurrences(of: "_", with: "-")
         // Some locale identifiers contain special fields like calendar or numbers, eg: "ar-SA@calendar=gregorian;numbers=latn"
-        return localeIdentifier.components(separatedBy: "@").first ?? localeIdentifier
+        (identifier.components(separatedBy: "@").first ?? identifier).replacingOccurrences(of: "_", with: "-")
     }
 }
