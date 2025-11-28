c4b50146d1743107125b38667c325c91b2c254bf
yuman <yuman@microsoft.com>
2025-11-28 16:46:58 +0800

    update code

--- SwiftCodeBook/Source/UseCase/Foundation/SerialTaskAndActor.swift
+++ SwiftCodeBook/Source/UseCase/Foundation/SerialTaskAndActor.swift
@@ -9,11 +9,13 @@ import Foundation
 
 // 当我们使用Task{}来包裹一个任务时，注意这个block可以被理解是放到了一个global queue来执行
 // 即如果我们依次创建TaskA/TaskB，但是实际内部block执行顺序是不确定的
-// 如果这两个task的创建处于不同线程，那倒还好，因为多线程时本来两者的创建时机也是不稳定的，我们不需要强行保证顺序执行
-// 但如果这两个task是在同一个线程依次创建，那我们可能就要看是否业务上要求两者必须严格按顺序执行了
+// 如果这两个task的创建处于不同线程，那倒还好，因为多线程时本来两者的创建时机也是不确定的，我们也不需要强行保证顺序执行
+// 但如果这两个task是在同一个线程依次创建，那我们就要看业务上是否要求两者必须严格按顺序执行了
 // 一个可能的场景：用户每输入一个字符我们都要发送一个网络请求，网络请求调用是一个await方法，需要放到Task里，且我们必须要保证网络请求是顺序的
 // 如果没有特殊处理，因为输入回调都是在主线程，即创建Task都是顺序的，但我们没法保证实际Task的执行顺序(即网络请求顺序)是串行的
-// 以下是使用AsyncStream来解决这个问题
+// 有一个简单的方法可以让这两个Task按顺序开始执行，即创建这些Task时这么写Task { @MainActor/@YourActor in ... }
+// 这样最简单，但缺点是TaskA的block里如果有await，则会让B开始执行。即这种写法只能保证A比B先开始执行，但不能保证A全部执行完才执行B
+// 如果想实现A全部执行完，再执行B，则可以使用AsyncStream实现
 
 final class SerialTaskAndActor {
     private let (stream, continuation) = AsyncStream<String>.makeStream()
