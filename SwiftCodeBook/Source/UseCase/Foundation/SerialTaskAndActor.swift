60b768066265c6bfaa5487459c63e1927cf7f325
yuman <yuman@microsoft.com>
2025-11-28 16:56:08 +0800

    update code

--- SwiftCodeBook/Source/UseCase/Foundation/SerialTaskAndActor.swift
+++ SwiftCodeBook/Source/UseCase/Foundation/SerialTaskAndActor.swift
@@ -15,6 +15,8 @@ import Foundation
 // 如果没有特殊处理，因为输入回调都是在主线程，即创建Task都是顺序的，但我们没法保证实际Task的执行顺序(即网络请求顺序)是串行的
 // 有一个简单的方法可以让这两个Task按顺序开始执行，即创建这些Task时这么写Task { @MainActor/@YourActor in ... }
 // 这样最简单，但缺点是TaskA的block里如果有await，则会让B开始执行。即这种写法只能保证A比B先开始执行，但不能保证A全部执行完才执行B
+// 另外如果有三个Task ABC，他们的block里都await actor.当A进入actor后，BC继续await。等A执行完后，BC哪个会被actor执行是不确定的。即Task的执行顺序对了，但是到了actor这一层又不一定对
+// 要解决这个case，需要让actor执行方法时不能有其他caller在await，即A全部执行完，B才开始执行
 // 如果想实现A全部执行完，再执行B，则可以使用AsyncStream实现
 
 final class SerialTaskAndActor {
