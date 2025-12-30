8ae0db8d10f90b51a62e8e94c63e8217bd0ccbb4
yuman <yuman@microsoft.com>
2025-12-30 16:15:07 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/UIKit/UIView+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/UIKit/UIView+Tools.swift
@@ -32,34 +32,3 @@ public extension UIView {
         return nil
     }
 }
-
-private extension UIGestureRecognizer {
-    enum AssociatedKeys {
-        static var action: Void?
-    }
-    
-    var action: ((UIGestureRecognizer) -> Void)? {
-        get { objc_getAssociatedObject(self, &AssociatedKeys.action) as? (UIGestureRecognizer) -> Void }
-        set { objc_setAssociatedObject(self, &AssociatedKeys.action, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
-    }
-}
-
-public extension UIView {
-    func addTapGesture(numberOfTapsRequired: Int = 1, action: @escaping (UIGestureRecognizer) -> Void) {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(gestureAction(gesture:)))
-        tap.numberOfTapsRequired = numberOfTapsRequired
-        tap.action = action
-        addGestureRecognizer(tap)
-    }
-    
-    func addLongPressGesture(action: @escaping (UIGestureRecognizer) -> Void) {
-        let long = UILongPressGestureRecognizer(target: self, action: #selector(gestureAction(gesture:)))
-        long.action = action
-        addGestureRecognizer(long)
-    }
-    
-    @objc
-    private func gestureAction(gesture: UIGestureRecognizer) {
-        gesture.action?(gesture)
-    }
-}
