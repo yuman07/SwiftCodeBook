8ec6c42cf399ddf523e70833412c63dfa622cda8
yuman <yuman@microsoft.com>
2026-01-04 15:06:18 +0800

    update code

--- SwiftCodeBook/Source/UseCase/UIKit/UITextView.clickSomeText.swift
+++ SwiftCodeBook/Source/UseCase/UIKit/UITextView.clickSomeText.swift
@@ -17,19 +17,21 @@ private final class ClickTextVC: UIViewController {
         guard case let range = (attString.string as NSString).range(of: "《个人信息保护法》"), range.isValid else { return }
         attString.addAttribute(.foregroundColor, value: UIColor.blue, range: range)
         textView.attributedText = attString
+        textView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapAction)))
         view.addSubview(textView)
+    }
+    
+    @objc private func tapAction(_ gr: UIGestureRecognizer) {
+        guard let textView = gr.view as? UITextView,
+              case let range = (textView.text as NSString).range(of: "《个人信息保护法》"), range.isValid,
+              case let location = gr.location(in: gr.view),
+              let start = textView.position(from: textView.beginningOfDocument, offset: range.location),
+              let end = textView.position(from: start, offset: range.length),
+              let textRange = textView.textRange(from: start, to: end),
+              case let rects = textView.selectionRects(for: textRange).map(\.rect),
+              rects.contains(where: { $0.contains(location) })
+        else { return }
         
-        textView.addTapGesture { gr in
-            guard let textView = gr.view as? UITextView,
-                  case let location = gr.location(in: gr.view),
-                  let start = textView.position(from: textView.beginningOfDocument, offset: range.location),
-                  let end = textView.position(from: start, offset: range.length),
-                  let textRange = textView.textRange(from: start, to: end),
-                  case let rects = textView.selectionRects(for: textRange).map(\.rect),
-                  rects.contains(where: { $0.contains(location) })
-            else { return }
-            
-            print("clicked")
-        }
+        print("clicked")
     }
 }
