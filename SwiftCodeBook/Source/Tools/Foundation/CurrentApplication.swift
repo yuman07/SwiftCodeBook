f6fdc6b043c8ac0ba684198f1383a8cc56866a4b
yuman <yuman@microsoft.com>
2026-01-19 15:31:07 +0800

    update code

---
+++ SwiftCodeBook/Source/Tools/Foundation/CurrentApplication.swift
@@ -0,0 +1,16 @@
+//
+//  CurrentApplication.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2026/1/19.
+//
+
+import Foundation
+
+public final class CurrentApplication {
+    private init() {}
+}
+
+public extension CurrentApplication {
+    
+}
