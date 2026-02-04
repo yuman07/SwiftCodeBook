7a8d7343c18fb3ebc88def504511ba0f29ad102f
yuman <yuman@microsoft.com>
2026-02-04 19:50:36 +0800

    update code

---
+++ SwiftCodeBook/Source/Tools/Extension/UIKit/UIViewController+Tools.swift
@@ -0,0 +1,35 @@
+//
+//  UIViewController+Tools.swift
+//  SwiftCodeBook
+//
+//  Created by yuman on 2026/2/4.
+//
+
+#if os(iOS) || os(tvOS) || os(visionOS)
+import UIKit
+
+public extension UIViewController {
+    func addChildSafely(_ child: UIViewController, layout: (_ child: UIViewController, _ parent: UIViewController) -> Void) {
+        guard child.parent != self else { return }
+        guard child.parent == nil else { return child.removeFromParentSafely() }
+        addChild(child)
+        view.addSubview(child.view)
+        layout(child, self)
+        child.didMove(toParent: self)
+    }
+    
+    func removeFromParentSafely() {
+        guard parent != nil else { return }
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+    
+    func removeAllChildren() {
+        while let last = children.last {
+            last.removeFromParentSafely()
+        }
+    }
+}
+
+#endif
