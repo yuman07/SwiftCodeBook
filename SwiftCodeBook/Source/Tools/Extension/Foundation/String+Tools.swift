3ad5aad88d1f46c0ade44ccaa93a45c0d37a9bb1
yuman <yuman@microsoft.com>
2026-02-13 16:56:28 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/String+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/String+Tools.swift
@@ -8,98 +8,6 @@
 import Foundation
 
 public extension String {
-    subscript(_ position: Int) -> Character? {
-        guard position >= 0, let index = index(startIndex, offsetBy: position, limitedBy: endIndex), index < endIndex else { return nil }
-        return self[index]
-    }
-    
-    subscript(_ range: ClosedRange<Int>) -> Substring? {
-        guard range.lowerBound >= 0,
-              let lowerIndex = index(startIndex, offsetBy: range.lowerBound, limitedBy: endIndex),
-              let upperIndex = index(startIndex, offsetBy: range.upperBound, limitedBy: endIndex),
-              lowerIndex < endIndex && upperIndex < endIndex
-        else { return nil }
-        return self[lowerIndex ... upperIndex]
-    }
-    
-    subscript(_ range: Range<Int>) -> Substring? {
-        guard range.lowerBound >= 0,
-              let lowerIndex = index(startIndex, offsetBy: range.lowerBound, limitedBy: endIndex),
-              let upperIndex = index(startIndex, offsetBy: range.upperBound, limitedBy: endIndex),
-              lowerIndex < endIndex && upperIndex <= endIndex
-        else { return nil }
-        return self[lowerIndex ..< upperIndex]
-    }
-    
-    // [0...]
-    subscript(_ range: PartialRangeFrom<Int>) -> Substring? {
-        guard range.lowerBound >= 0,
-              let lowerIndex = index(startIndex, offsetBy: range.lowerBound, limitedBy: endIndex),
-              lowerIndex < endIndex
-        else { return nil }
-        return self[lowerIndex...]
-    }
-    
-    // [..<2]
-    subscript(_ range: PartialRangeUpTo<Int>) -> Substring? {
-        guard range.upperBound > 0,
-              let upperIndex = index(startIndex, offsetBy: range.upperBound, limitedBy: endIndex),
-              upperIndex <= endIndex
-        else { return nil }
-        return self[..<upperIndex]
-    }
-    
-    // [...2]
-    subscript(_ range: PartialRangeThrough<Int>) -> Substring? {
-        guard range.upperBound >= 0,
-              let upperIndex = index(startIndex, offsetBy: range.upperBound, limitedBy: endIndex),
-              upperIndex < endIndex
-        else { return nil }
-        return self[...upperIndex]
-    }
-}
-
-public extension StringProtocol {
-    // 对给定的index进行移动，正数向后，负数向前，最终结果在[startIndex, endIndex)即有效
-    func indexSafely(_ i: Index, offsetBy distance: Int) -> Index? {
-        var currentIndex = i
-        var count = abs(distance)
-        
-        while count > 0 {
-            if distance > 0 {
-                guard currentIndex < endIndex else { return nil }
-                currentIndex = index(after: currentIndex)
-            } else {
-                guard currentIndex > startIndex else { return nil }
-                currentIndex = index(before: currentIndex)
-            }
-            count -= 1
-        }
-        
-        guard currentIndex >= startIndex && currentIndex < endIndex else { return nil }
-        return currentIndex
-    }
-    
-    func forEachWithIndexAndChar(_ body: (Index, Character) throws -> Void) rethrows {
-        var currentIndex = startIndex
-        while currentIndex < endIndex {
-            try body(currentIndex, self[currentIndex])
-            currentIndex = index(after: currentIndex)
-        }
-    }
-    
-    func ranges<T>(of aString: T, options: String.CompareOptions = [], locale: Locale? = nil) -> [Range<String.Index>] where T: StringProtocol {
-        var ranges = [Range<String.Index>]()
-        var lastUpperBound = startIndex
-        while lastUpperBound < endIndex, let range = range(of: aString, options: options, range: lastUpperBound ..< endIndex, locale: locale) {
-            ranges.append(range)
-            lastUpperBound = range.upperBound
-        }
-        return ranges
-    }
-}
-
-public extension String {
     func validatedRange(_ range: Range<Index>) -> Range<Index>? {
         guard let lower = Index(range.lowerBound, within: self),
               let upper = Index(range.upperBound, within: self),
@@ -145,6 +53,46 @@ public extension String {
               let end = Index(upper, within: self), end >= startIndex, end <= endIndex, from < end
         else { return nil }
         return from ..< end
+    }
+}
+
+public extension StringProtocol {
+    // 对给定的index进行移动，正数向后，负数向前，最终结果在[startIndex, endIndex)即有效
+    func indexSafely(_ i: Index, offsetBy distance: Int) -> Index? {
+        var currentIndex = i
+        var count = abs(distance)
+        
+        while count > 0 {
+            if distance > 0 {
+                guard currentIndex < endIndex else { return nil }
+                currentIndex = index(after: currentIndex)
+            } else {
+                guard currentIndex > startIndex else { return nil }
+                currentIndex = index(before: currentIndex)
+            }
+            count -= 1
+        }
+        
+        guard currentIndex >= startIndex && currentIndex < endIndex else { return nil }
+        return currentIndex
+    }
+    
+    func forEachWithIndexAndChar(_ body: (Index, Character) throws -> Void) rethrows {
+        var currentIndex = startIndex
+        while currentIndex < endIndex {
+            try body(currentIndex, self[currentIndex])
+            currentIndex = index(after: currentIndex)
+        }
+    }
+    
+    func ranges<T>(of aString: T, options: String.CompareOptions = [], locale: Locale? = nil) -> [Range<String.Index>] where T: StringProtocol {
+        var ranges = [Range<String.Index>]()
+        var lastUpperBound = startIndex
+        while lastUpperBound < endIndex, let range = range(of: aString, options: options, range: lastUpperBound ..< endIndex, locale: locale) {
+            ranges.append(range)
+            lastUpperBound = range.upperBound
+        }
+        return ranges
     }
 }
 
