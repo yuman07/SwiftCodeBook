8301c29608301eeb961bd08c9ba3e49bf3290b76
yuman <yuman@microsoft.com>
2026-02-16 13:59:55 +0800

    update code

--- SwiftCodeBook/ViewController.swift
+++ SwiftCodeBook/ViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let exe = SerialTaskExecutor()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        exe.async {
-            print("123")
-        }
+        
     }
 }
