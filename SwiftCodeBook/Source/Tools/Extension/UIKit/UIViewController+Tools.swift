d7c727d8272f883c3a974d2d0407ca8715b8b554
yuman <yuman@microsoft.com>
2026-03-11 11:30:14 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/UIKit/UIViewController+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/UIKit/UIViewController+Tools.swift
@@ -11,7 +11,7 @@ import UIKit
 public extension UIViewController {
     func addChildSafely(_ child: UIViewController, layout: @MainActor (_ child: UIViewController, _ parent: UIViewController) -> Void) {
         guard child.parent != self else { return }
-        guard child.parent == nil else { return child.removeFromParentSafely() }
+        if child.parent != nil { child.removeFromParentSafely() }
         addChild(child)
         view.addSubview(child.view)
         layout(child, self)
