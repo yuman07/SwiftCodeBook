bbe8c862cb1ecdc69d258513cf12077a342c1006
yuman <yuman@microsoft.com>
2026-03-23 16:32:13 +0800

    update code

--- SwiftCodeBook/Source/Tools/Foundation/CurrentApplication.swift
+++ SwiftCodeBook/Source/Tools/Foundation/CurrentApplication.swift
@@ -31,8 +31,8 @@ public extension CurrentApplication {
         UIApplication.shared
             .connectedScenes
             .compactMap { $0 as? UIWindowScene }
-            .flatMap { $0.windows }
-            .first { $0.isKeyWindow }
+            .compactMap { $0.keyWindow }
+            .first
     }
 #endif
     
