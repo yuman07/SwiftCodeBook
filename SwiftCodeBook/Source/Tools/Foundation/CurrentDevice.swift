ba947df6356a22c6d9df1b14023c9f6e435ec3c3
yuman <yuman@microsoft.com>
2026-04-02 16:21:12 +0800

    update code

--- SwiftCodeBook/Source/Tools/Foundation/CurrentDevice.swift
+++ SwiftCodeBook/Source/Tools/Foundation/CurrentDevice.swift
@@ -105,11 +105,9 @@ public extension CurrentDevice {
         (try? FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory()))?[.systemSize] as? UInt64
     }()
     
-//    static var freeDiskSpaceInByte: UInt64? {
-//        let url = URL(fileURLWithPath: NSHomeDirectory())
-//        let values = try? url.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
-//        return values?.volumeAvailableCapacityForImportantUsage.flatMap { $0 >= 0 ? UInt64($0) : nil }
-//    }
+    static var freeDiskSpaceInByte: UInt64? {
+        (try? FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory()))?[.systemFreeSize] as? UInt64
+    }
 }
 
 public extension CurrentDevice {
--- Watch Watch App/ContentView.swift
+++ Watch Watch App/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by yuman on 2026/4/2.
 //
 
+import WatchKit
 import SwiftUI
 
 struct ContentView: View {
@@ -16,5 +17,8 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            WKInterfaceDevice.current().play(.retry)
+        }
     }
 }
