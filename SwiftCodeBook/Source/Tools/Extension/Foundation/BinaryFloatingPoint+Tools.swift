c70817b866bb5433621d42a6f02d5fc1f17a81ce
yuman <yuman@microsoft.com>
2026-04-05 13:33:11 +0800

    update code

--- SwiftCodeBook/Source/Tools/Extension/Foundation/BinaryFloatingPoint+Tools.swift
+++ SwiftCodeBook/Source/Tools/Extension/Foundation/BinaryFloatingPoint+Tools.swift
@@ -10,8 +10,17 @@ import Foundation
 public extension BinaryFloatingPoint {
     func equals(_ other: Self, tolerance: Self = Self.ulpOfOne.squareRoot()) -> Bool {
         if self == other { return true }
-        guard isFinite && other.isFinite else { return false }
+        if isNaN || other.isNaN { return false }
+        if isInfinite || other.isInfinite { return self == other }
         return abs(self - other) <= abs(tolerance)
+    }
+    
+    func greaterThan(_ other: Self, tolerance: Self = Self.ulpOfOne.squareRoot()) -> Bool {
+        self > other && !equals(other, tolerance: tolerance)
+    }
+    
+    func lessThan(_ other: Self, tolerance: Self = Self.ulpOfOne.squareRoot()) -> Bool {
+        self < other && !equals(other, tolerance: tolerance)
     }
     
     func greaterThanOrEquals(_ other: Self, tolerance: Self = Self.ulpOfOne.squareRoot()) -> Bool {
